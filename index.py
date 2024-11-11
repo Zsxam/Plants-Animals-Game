cce015502686825b8166e8ac38817fb6b1b9b0f1
Sina-32 <357.skbm@gmail.com>
2024-11-11 14:36:37 +0700

    Menambah Login

--- index.py
+++ index.py
@@ -4,15 +4,39 @@ import random
 # File untuk menyimpan data guru dan kelas
 DATA_FILE = "data.json"
 # File yang berisi informasi hewan dan tumbuhan
-SUBJECTS_FILE = "subjects.json"
+SUBJECTS_FILE = "Plants-Animals-Game\subjects.json"
 # File yang berisi pertanyaan kuis
-QUIZ_FILE = "quiz_questions.json"
+QUIZ_FILE = "Plants-Animals-Game\quiz_questions.json"
 
 def register_teacher():
     pass
 
 def login_teacher():
-    pass
+
+    print("Silahkan Login")
+
+    username_terdaftar = "Daspro2024"
+    password_terdaftar = "Latihan"
+    kesempatan = 5
+
+    while kesempatan > 0:
+        username = input("Masukkan username: ")
+        password = input("Masukkan password: ")
+
+        if username == username_terdaftar and password == password_terdaftar:
+            print("Login berhasil!")
+            return
+
+        elif username == username_terdaftar:
+            print(f"Password salah. Kesempatan tersisa: {kesempatan-1}")
+        elif password == password_terdaftar:
+            print(f"Username salah. Kesempatan tersisa: {kesempatan-1}")
+        else:
+            print(f"Username dan password salah. Kesempatan tersisa: {kesempatan-1}")
+
+        kesempatan -= 1
+
+    print("Anda telah keluar dari sistem login")
 
 # Load data dari file JSON
 def load_data(filename):
