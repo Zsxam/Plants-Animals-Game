b8dba7970d02c1abb53d579c09e9f3efc9c02094
azizaputriamelia <@azizaputriamelia19@gmail.com>
2024-11-25 17:48:10 +0700

    Menambahkan kode .strip()

--- logout.py
+++ logout.py
@@ -2,7 +2,7 @@ pengguna_login = True
 
 print("Menu Logout")
 if pengguna_login:
-    pilih = input("Apakah anda ingin logout? (ya/tidak): ").lower()
+    pilih = input("Apakah anda ingin logout? (ya/tidak): ").strip().lower()
     if pilih == "ya":
         pengguna_login = False
         print("Logout berhasil!")
